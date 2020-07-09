d3882a5c539c2d808204020c0623f0357b8925b5
Manon Morgaut <manonmorgaut@gmail.com>
2020-07-09 15:51:25 +0200

    Debug

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", (req, res) => {
-  res.send("foo");
+  res.render("index");
 });
 
 router.get("/sneakers/:cat", (req, res) => {
-  res.send("bar");
+  res.render("/products");
 });
 
 router.get("/one-product/:id", (req, res) => {
