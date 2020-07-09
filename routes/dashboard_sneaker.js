429e98bd0c22e8d91535903c5874c1e54998c843
Cha Noguer <definitely.not.a.chatbot@gmail.com>
2020-07-09 17:20:37 +0200

    added a seed.js inside bin

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -3,8 +3,11 @@ const router = new express.Router(); // create an app sub-module (router)
 const sneakerModel = require("./../models/Sneaker");
 const uploader = require("./../config/cloudinary");
 
+//ALL ROUTES ARE PREFIXED WITH /dashboard_sneaker
 
 // add routes = add, delete, update
+
+
 
 
 module.exports = router;
