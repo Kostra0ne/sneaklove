543f5d12cf96cb0a473f9460c5c47483257037db
Spyros Kyritsis <spyros.kyritsis@gmail.com>
2020-07-10 12:53:36 +0200

    sneakers 10-07

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,30 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
+const sneakerModel = require("../models/Sneaker");
+
+router.get("/collection", (req, res, next) => {
+    sneakerModel.find()
+        .then((sneakers) => {
+        res.render("products", {sneakers})
+        })
+        .catch(next);
+
+});
+
+router.get("/:cat", (req, res,next) => {
+   sneakerModel.find(
+        {category: req.params.cat}
+   ).then((sneakers) => {
+    res.render("products", {sneakers} ); 
+   })
+    .catch(next)
+    
+});
+  
+
+
+
+
+
 
 module.exports = router;
