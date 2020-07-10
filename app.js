4e391b0292d7055f6bb38f8eb5b373ee3e97fc4b
abWasHere <audrey.belson@gmail.com>
2020-07-10 13:10:24 +0200

    working on the delete/edit routes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-const dev_mode = false; // TODO WARNING
+const dev_mode = true; // TODO WARNING
 const logger = require("morgan");
 
 app.use(express.urlencoded({ extended: true }));
@@ -69,5 +69,7 @@ app.use(require("./middlewares/exposeFlashMessage"));
 // routers
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/dashboard", require("./routes/dashboard_sneaker"));
+
 
 module.exports = app;
--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,66 @@
-const express = require("express"); // import express in this module
-const router = new express.Router(); // create an app sub-module (router)
+const express = require("express");
+const router = new express.Router();
+const userModel = require("./../models/User");
+const sneakerModel = require("./../models/Sneaker");
+const tagModel = require("./../models/Tag");
+const { log } = require("debug");
+const uploader = require("./../config/cloudinary");
+const { render } = require("../app");
+
+/* Routes are prefixed with dashboard/ */
+
+router.get("/prod-add", (req, res, next) => {
+   tagModel
+      .find()
+      .then((allTags) => {
+         res.render("products_add", { allTags });
+      })
+      .catch(next);
+});
+
+router.get("/prod-manage", (req, res, next) => {
+   sneakerModel
+      .find()
+      .then((sneakers) => {
+         res.render("products_manage", { sneakers });
+      })
+      .catch(next);
+});
+
+router.post("/prod-add", uploader.single("image"), (req, res, next) => {
+   const newProd = req.body;
+
+   console.log(req.file);
+   console.log(req.body);
+
+   if (req.file) newProd.image = req.file.path;
+
+   sneakerModel
+      .create(newProd)
+      .then(() => {
+         req.flash("success", "sneaker successfully created");
+         res.redirect("/dashboard/prod-manage");
+      })
+      .catch(next);
+});
+
+router.get("/prod-edit/:id", (req, res, next) => {
+   sneakerModel
+      .findById(req.params.id)
+      .then((sneaker) => {
+         res.render("product_edit", sneaker);
+      })
+      .catch(next);
+});
+
+router.get("/prod-delete/:id", (req, res, next) => {
+   sneakerModel
+      .findByIdAndDelete(req.params.id)
+      .then(() => {
+         req.flash("success", "product successfully deleted");
+         res.redirect("/prod-manage");
+      })
+      .catch(next);
+});
 
 module.exports = router;
