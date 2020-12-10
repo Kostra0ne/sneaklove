ee14b53a9d0a3c4a42bc29ad0d670880c045c505
Victorien Provenzano <victorien.provenzano@icloud.com>
2020-12-10 19:13:54 +0100

    sign in to be done

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const sneakerSchema = new Schema(
   {
     name: String,
     ref: String,
-    size: String,
-    street: String,
-    description: Number,
-    price: String,
+    size: Number,
+    description: String,
+    price: Number,
     category: { type: String, enum: ["men", "women", "kids"] },
-    // id_tags: [ObjectId],
+    image: String,
+    id_tags: [{ type: Schema.Types.ObjectId, ref: "label" }],
   },
   { timestamps: true }
 );
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,55 @@
 const express = require("express");
 const router = new express.Router();
-// const SneakerModel = require("./../models/Sneaker");
-// const UserModel = require("./../models/User");
-// const TagsModel = require("./../models/Tag");
+const SneakerModel = require("./../models/Sneaker");
+const UserModel = require("./../models/User");
+const TagModel = require("./../models/Tag");
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/sneakers/:cat", (req, res) => {
+router.get("/sneakers/collection", async (req, res, next) => {
   try {
-    // await SneakerModel.find(req.params.cat);
-    res.render("products");
+    const sneakers = await SneakerModel.find();
+    const tags = await TagModel.find();
+    res.render("products", { sneakers, tags });
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/one-product/:id", (req, res) => {
-  res.render("baz");
+router.get("/sneakers/:cat", async (req, res, next) => {
+  try {
+    const category = req.params.cat;
+    const sneakers = await SneakerModel.find({ category });
+    const tags = await TagModel.find();
+    res.render("products", { category, sneakers, tags });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/one-product/:id", async (req, res, next) => {
+  try {
+    res.render("one_product", await SneakerModel.findById(req.params.id));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/sneakers/collection/:id", async (req, res, next) => {
+  const tag = await TagModel.findById(req.params.id);
+  const tags = await TagModel.find();
+  const sneakers = await SneakerModel.find({ id_tags: tag });
+  res.render("products", { sneakers, tags });
 });
 
 router.get("/signup", (req, res) => {
-  res.render("sneak");
+  res.render("signup");
 });
 
 router.get("/signin", (req, res) => {
-  res.render("love");
+  res.render("signin");
 });
 
 module.exports = router;
