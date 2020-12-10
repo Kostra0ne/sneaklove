9d160960e9bd8cb7a98553c0dbf642d31b1823b9
Thomas Lamy <thomaslamy75011@gmail.com>
2020-12-10 15:22:55 +0100

    debuggind done

---
+++ models/Sneaker.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const sneakerSchema = new Schema(
+{
+name: String,
+ref: String,
+size: Number,
+description: String,
+price: Number,
+category: {type: String, enum : ["men", "women", "kids"]
+},
+id_tags: Schema.Types.ObjectId
+}
+)
+
+const SneakersModel = mongoose.model("sneakers", sneakerSchema),
