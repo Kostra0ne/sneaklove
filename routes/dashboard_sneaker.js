88955a1d7f135a6281aa40d7ca16dcfdb2044f92
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-12-11 13:04:56 +0100

    dashboard

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,9 +1,20 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
 
-module.exports = router;
+const SneakerModel = require("../models/Sneaker");
+const UserModel = require("../models/User");
+const TagModel = require("../models/Tag");
 
 
-router.get(["/titi"], async (req, res, next) => {
-    res.render("patial/dashboard_sneaker_row")
+router.get("/dashboard", async (req, res, next) => {
+    const sneakers = await SneakerModel.find()
+    console.log(sneakers);
+    //              view        +    object
+    res.render("products_manage", {sneakers} )
   });
+
+
+
+
+
+  module.exports = router;
