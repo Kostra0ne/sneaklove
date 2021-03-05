b97192279b5c9e4b69cf4fcb4d292f2ccc6344e1
Amalia Zebi <amalia.zebi@b4finance.com>
2021-03-05 15:40:19 +0100

    Message salt fixed

--- routes/index.js
+++ routes/index.js
@@ -67,26 +67,26 @@ router.get("/signup", (req, res) => {
 
 router.post("/signup", async (req, res, next) => {
   try {
-    const newUser = {...req.body };
-    const foundUser = await UsersModel.findOne({email: newUser.email});
-    if(foundUser){
-      req.flash('warning','email already in use.');
-      res.redirect('/signin')
-      console.log("Already in the DB");
-    }else {
-      console.log("NewUSer Password" + newUser);
-      const hashedPassword= bcrypt.hashSync(newUser.password, 10)
-      
-      newUser.password = hashedPassword;
-      
+    const newUser = { ...req.body };
+    const foundUser = await UsersModel.findOne({ email: newUser.email });
 
+    if (foundUser) {
+      req.flash("warning", "Email already registered");
+      res.redirect("/signup");
+    } else {
+      const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+      newUser.password = hashedPassword;
       await UsersModel.create(newUser);
-      res.redirect('/signin')
-      console.log("New User" + newUser)
+      req.flash("success", "Congrats ! You are now registered !");
+      res.redirect("/signin");
     }
-  } catch(err) {
-    next(err)
-    console.log(err);
+  } catch (err) {
+    let errorMessage = "";
+    for (field in err.errors) {
+      errorMessage += err.errors[field].message + "\n";
+    }
+    req.flash("error", errorMessage);
+    res.redirect("/signup");
   }
 
 });
