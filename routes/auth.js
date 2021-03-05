7d5ffbc902d46950ea1656de7da5881e2e97b3d7
Anaïs Engler <a.engler@outlook.fr>
2021-03-05 11:33:30 +0100

    auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const User = require("./../models/User");
+const bcrypt = require("bcrypt");
+const UserModel = require("./../models/User");
+
+
+//GET ROUTES
+router.get("/signin", (req, res, next) => {
+    res.render("signin");
+});
+
+router.get("/signup", (req, res, next) => {
+    res.render("signup");
+});
+
+router.get("/signout", (req, res, next) => {
+    req.session.destroy(function (err) {
+        res.redirect("/signin");
+    });
+});
+
+//POST ROUTES
+router.post("/signin", async (req, res, next) => {
+    //Your code here
+});
+
+router.post("/signup", async (req, res, next) => {
+    try {
+        const newUser = {name, lastname, email, password};
+        const foundUser = await UserModel.findOne({email: newUser.email});
+    
+    if(foundUser) {
+        req.flash("warning", "Email already registered");
+        res.redirect("/signup");
+    } else {
+        const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+        newUser.password = hashedPassword;
+        await UserModel.create(newUser);
+        req.flash("success", "Congrats! You are now registered!");
+        res.redirect("/signin");
+    }
+    } catch (err) {
+        let errorMessage = "";
+        for (field in err.errors){
+            errorMessage += err.errors[field].message + "\n";
+        }
+        req.flash("error", errorMessage);
+        res.redirect("/signup");
+    }
+});
+
+
+
 module.exports = router;
+
+
+
