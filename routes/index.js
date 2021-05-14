76810ed925f75af20e9107b5e44a437e116021c2
Stephane Mbesse <stephane.mbesse@gmail.com>
2021-05-14 13:26:20 +0200

    Debuging app.js and index.js

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-return console.log(`\n\n
------------------------------
------------------------------
-     wax on / wax off !
------------------------------
------------------------------\n\n`
-);
+// return console.log(`\n\n
+// -----------------------------
+// -----------------------------
+//      wax on / wax off !
+// -----------------------------
+// -----------------------------\n\n`
+// );
 
 router.get("/", (req, res) => {
-  res.send("foo");
+  res.render("index");
 });
 
 router.get("/sneakers/:cat", (req, res) => {
-  res.send("bar");
+  res.render("products");
 });
 
 router.get("/one-product/:id", (req, res) => {
-  res.send("baz");
+  res.render("one_product");
 });
 
 router.get("/signup", (req, res) => {
-  res.send("sneak");
+  res.render("signup");
 });
 
 router.get("/signin", (req, res) => {
-  res.send("love");
+  res.render("signin");
 });
 
 
